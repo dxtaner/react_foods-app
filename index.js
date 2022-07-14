5423408d5f160cb32f48667a085befc4cf3f20d9
Taner Özer <tanerozer16@gmail.com>
2022-07-14 23:12:37 +0300

    env add get request add

--- index.js
+++ index.js
@@ -2,14 +2,30 @@ const Port = 8080;
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const fetch =  require("node-fetch");
+const { response, json } = require("express");
+// const fetch =  require("node-fetch");
 
 require("dotenv").config();
 
-morgan("tiny");
 
 const app = express();
 
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
+
+// get foods
+app.get("/foods", () => {
+    const url = process.env.ENDPOINT
+    const options = {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            'X-Cassandra-Token': process.env.ASTRA_TOKEN
+        }
+    }
+    fetch(url, options).then(response => response.json()).then(json => res.json(json)).catch(err => console.log("error:" + err));
+});
+
+
+app.listen(Port, () => console.log(`server is running ${Port}`));
