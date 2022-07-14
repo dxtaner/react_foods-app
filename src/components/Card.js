3785a7dca6e9ec059491dcd7692e472c4f8cfe95
Taner Özer <tanerozer16@gmail.com>
2022-07-14 23:39:18 +0300

    card js edited

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,13 @@
-const Card = () =>{
-    return <div className="card"></div>
+const Card = ({food}) =>{
+    
+    return (  <div className="card">
+        <h2> {food.name} </h2>
+        <p>{food.description}</p>
+        <p>{food.location.address}</p>
+        <p>{food.location.web}</p>
+        {food.visited && <div className="visited"></div>}
+        {!food.visited && <div className="not-visited"></div>}
+    </div> )
 }
 
 export default Card;
